ed1b2f7381e289816efd254cf5738e61aa3e38d1
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-07-14 11:58:46 -0300

    add categories

--- src/App.vue
+++ src/App.vue
@@ -13,33 +13,92 @@
         </div>
       </div>
       <div class="flex justify-center py-4">
-        <button
-          @click="changeFrase()"
-          class="bg-blue-700 hover:bg-blue-500 rounded-lg text-white font-bold py-4 px-10 mb-10 text-2xl"
-        >
-          Puxar assunto
-        </button>
+        <div class="flex flex-col">
+          <button
+            @click="changeFrase()"
+            class="bg-blue-700 hover:bg-blue-500 rounded-lg text-white font-bold py-4 px-10 mb-10 text-2xl"
+          >
+            Puxar assunto
+          </button>
+          <select
+            v-model="selectedCategory"
+            class="p-4 border border-1 border-slate-500 bg-white rounded-lg"
+            name="category"
+            id=""
+          >
+            <option value="">Selecione uma categoria</option>
+            <option
+              v-for="(category, index) in categories"
+              :value="index"
+              :key="index"
+            >
+              {{ category.category_name }}
+            </option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
+import { useHead } from "@vueuse/head";
 import questions from "./data/questions.json";
 
-let frase = ref("Clique no botão para puxar um assunto!");
-
-const list = questions.categories[0].questions;
+let frase = ref("Clique no botão para puxar um assunto!");
+const answeredQuestions = ref([]);
+const selectedCategory = ref("");
+const categories = questions.categories;
+let unansweredQuestions = ref([]);
 
 function changeFrase() {
-  frase.value = list[Math.floor(Math.random() * list.length)];
+  // Verificar se uma categoria foi selecionada
+  if (selectedCategory.value === "") {
+    frase.value = "Selecione uma categoria antes de puxar um assunto!";
+    return;
+  }
+
+  const categoryIndex = parseInt(selectedCategory.value);
+  const categoryQuestions = categories[categoryIndex].questions;
+
+  // Verificar se todas as perguntas da categoria foram respondidas
+  if (unansweredQuestions.value.length === 0) {
+    frase.value = "Todas as perguntas desta categoria foram respondidas!";
+    return;
+  }
+
+  const randomIndex = Math.floor(
+    Math.random() * unansweredQuestions.value.length
+  );
+  const selectedQuestion = unansweredQuestions.value[randomIndex];
+
+  frase.value = selectedQuestion;
+  answeredQuestions.value.push(selectedQuestion);
+  unansweredQuestions.value.splice(randomIndex, 1);
 }
+
+function updateUnansweredQuestions() {
+  if (selectedCategory.value === "") {
+    unansweredQuestions.value = [];
+    return;
+  }
+
+  const categoryIndex = parseInt(selectedCategory.value);
+  const categoryQuestions = categories[categoryIndex].questions;
+
+  unansweredQuestions.value = categoryQuestions.filter(
+    (question) => !answeredQuestions.value.includes(question)
+  );
+}
+
+onMounted(() => {
+  useHead({
+    title: "Puxa Assunto",
+  });
+});
+
+watch(selectedCategory, updateUnansweredQuestions);
 </script>
 
-<style>
-/* body {
-  background-color: #025464;
-  color: #f8f1f1;
-} */
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const head = createHead()
+
+createApp(App).use(head).mount('#app')
