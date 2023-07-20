545dd7d5210169882cea01037a6b35a199e6b3d4
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-07-19 21:54:18 -0300

    vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
+import { inject } from '@vercel/analytics';
 
 import './style.css'
 import App from './App.vue'
@@ -7,3 +8,5 @@ import App from './App.vue'
 const head = createHead()
 
 createApp(App).use(head).mount('#app')
+
+inject();
